41f8c557aa204717b4fe46c79c318c892345f9bc
Matthew Batchelder <borkweb>
2012-09-05 17:58:40 -0400

    Adding license and misc info to plugin header

--- wp-debug-robot.php
+++ wp-debug-robot.php
@@ -6,6 +6,12 @@ Plugin URI: http://borkweb.com
 Description: Send debug output via UDP packets to a nodejs jabber bot that will IM you.
 Author: Matthew Batchelder
 Author URI: http://borkweb.com
+Contributors: borkweb, abackstrom
+Tags: debug, debugging, jabber, robot, udp
+Tested up to: 3.4.1
+Stable tag: 3.4
+License: MIT
+License URI: http://opensource.org/licenses/mit-license.php
 */
 
 require_once __DIR__ . '/lib/class-debugrobot-robotinterface.php';
